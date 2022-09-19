35c5038bd1a5189cb662988e7d4071baae442d8f
gjpro <wangrenkai@chinasofti.com>
2022-09-19 11:25:12 +0800

    dev test

---
+++ lib/page/rs/rs_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'rs_controller.dart';
+
+class RsPage extends StatelessWidget {
+  final controller = Get.put(RsController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
