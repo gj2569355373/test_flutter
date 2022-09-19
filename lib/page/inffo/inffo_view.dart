b35ab63dfbe15cad267b741866c2372867324b1f
gjpro <wangrenkai@chinasofti.com>
2022-09-19 11:29:57 +0800

    ap66 test

---
+++ lib/page/inffo/inffo_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'inffo_controller.dart';
+
+class InffoPage extends StatelessWidget {
+  final controller = Get.put(InffoController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/page/inoff/inoff_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'inoff_controller.dart';
+
+class InoffPage extends StatelessWidget {
+  final controller = Get.put(InoffController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
